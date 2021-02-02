72903aeecfee158c4ab828b7cf552bf54efb28df
michael-fore <michael-fore@sbcglobal.net>
2021-02-01 18:32:41 -0600

    move file to be more intuitive in a reallife setting

---
+++ app_2.py
@@ -0,0 +1,25 @@
+from dash import Dash
+import dash_core_components as dcc 
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+df = pd.DataFrame({'Tako':[1, 2], 
+					'Cats':[3,4]})
+
+#simulates two different dash apps
+#requests pathname prefix tells the dash renderer where to look and request data
+
+#dummy app 2
+app2 = Dash(__name__,
+			requests_pathname_prefix='/app2/')
+
+
+app2.layout = html.Div([
+	html.H1('App 2'),
+	dcc.Graph(id='id',
+			figure=px.scatter(df, 'Tako', 'Cats'))]
+	)
+	
+if __name__ == '__main__':
+	app2.run_server(debug=True)
--- router.py
+++ router.py
@@ -1,5 +1,6 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from my_app import app1, app2
+from app_1 import app1
+from app_2 import app2
 from flask import Flask
 
 base_app = Flask(__name__)
